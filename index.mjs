a9427e69834681c1d4ed05319b653f369a04e5e5
Dmitry Krechetov <krechetov52@gmail.com>
2023-09-28 17:16:58 +0200

    Init

---
+++ index.mjs
@@ -0,0 +1,44 @@
+import AWS from 'aws-sdk';
+import nodemailer from 'nodemailer';
+
+const ses = new AWS.SES({ region: 'ap-southeast-1' });
+
+const handler = async (event) => {
+    const bodyobject = JSON.parse(event.body);
+    const email = bodyobject.data.object.billing_details.email;
+    const name = bodyobject.data.object.billing_details.name;
+    const price = bodyobject.data.object.amount;
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ionos.de', // SMTP server address
+        port: 587, // SMTP server PORT 
+        secure: false, // Set 'true' if use SSL/TLS
+        auth: {
+            user: '', // SMTP login
+            pass: '', // SMTP password
+        },
+    });
+
+    const mailOptions = {
+        from: 'support@talentinsight-europe.com', // Sender email
+        to: email, // Reciever email
+        subject: 'Proof of payment',
+        text: `Your payment in the amount of $${price / 100} has been successfully processed. Payment details: ${bodyobject.data.object.receipt_url}`,
+    };
+    try {
+        const info = await transporter.sendMail(mailOptions);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Email sended', info })
+        };
+    } catch (error) {
+        console.error('Fail email sending', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Fail email sending' })
+        };
+    }
+};
+
+export { handler };
